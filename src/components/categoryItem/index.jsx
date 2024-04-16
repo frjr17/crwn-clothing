8fc4e5f02c867a735e5fbd74c193dbd8583c1444
Hernán Valencia <hernanadrianv17@gmail.com>
2024-04-16 14:55:08 -0500

    🚀 init: Initial commit

---
+++ src/components/categoryItem/index.jsx
@@ -0,0 +1,14 @@
+import './categoryItem.styles.scss'
+export default function CategoryItem({ category }) {
+    return (
+        <div key={category.id} className="category-container">
+            <div className="background-image" style={{
+                backgroundImage: `url(${category.imageUrl})`
+            }} />
+            <div className="category-body-container">
+                <h2>{category.title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    );
+}
